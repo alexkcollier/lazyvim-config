3b0a3e75626a7fde8496e97e756774d06ffb8f67
Alex Collier <alexkcollier@gmail.com>
2024-04-04 10:01:25 -0400

    Adds new themes and changes default

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,15 +2,35 @@ return {
   -- add catppuccin
   {
     "catppuccin/nvim",
-    lazy = true,
+    lazy = false,
     name = "catppuccin",
+  },
+
+  -- add tokyonight
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+  },
+
+  -- add kanagawa
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = false,
+    name = "kanagawa",
+  },
+
+  -- add nightfly
+  {
+    "bluz71/vim-nightfly-colors",
+    name = "nightfly",
+    lazy = false,
   },
 
   -- Configure active theme
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin-macchiato",
+      colorscheme = "tokyonight-storm",
     },
   },
 }
